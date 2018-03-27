a3187abff54226bed404bbc22c332bdeb3864d65
chrisperez626 <chrisperez626@gmail.com>
2018-03-27 19:39:34 -0400

    initial program files

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
+// original array for gifs
 var gifs = [];
-
+// function to create buttons from 
 function renderButtons() {
 
-        // Deletes the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-display").empty();
-        // Loops through the array of movies
+        
     for (var i = 0; i < gifs.length; i++) {
         const element = gifs[i];
         
-
         var button = $("<button>");
-
-        // Adds a class of movie to our button
-        button.addClass("gif");
-        // Added a data-attribute
+        
         button.attr("data-gif", element);
-        // Provided the initial button text
+        
         button.text(element);
-        // Added the button to the buttons-view div
+        
         $("#buttons-display").append(button);
     }
 };
 
-renderButtons();
-
+// adds input from search bar into array of gifs
 $("body").on("click", "#add-gif", function(event) {
     event.preventDefault();
-    // This line of code will grab the input from the textbox
+    
     var addedGif = $("#gif-searchbar").val()
-    // The movie from the textbox is then added to our array
+
     gifs.push(addedGif);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
 
-    // $("#gif-searchbar").val("");
+    $("#gif-searchbar").val("");
 });
 
-
+// once button is clicked this brings up gifs along with set attributes
 $("body").on("click","button", function() {
     var newGif = $(this).attr("data-gif");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -59,15 +52,29 @@ $("body").on("click","button", function() {
 
             var gifImage = $("<img>");
             gifImage.attr("src", results[i].images.fixed_height.url);
-
+            gifImage.addClass("gif");
+            gifImage.attr("still", results[i].images.fixed_height_still.url);
+            gifImage.attr("animated", results[i].images.fixed_height.url);
+            gifImage.attr("state", "animated");
             $("#gif-display").prepend(p);
             $("#gif-display").prepend(gifImage);
-        console.log(results);
         }
       });
 });
-$("body").on("click", "img", function(){
+//pauses gifs
+$("body").on("click", ".gif", function(){
     
+    var state = $(this).attr("state");
+    // console.log(state);
+    if(state === "animated"){
+        $(this).attr("src", $(this).attr("still"));
+        $(this).attr("state", "still");
+    }
+    if(state === "still"){
+        $(this).attr("src", $(this).attr("animated"));
+        $(this).attr("state", "animated");
+    };
 });
+
 
 
